d1466f18ab921b6052cc3b971089b8ea46086a81
poochingil <siddhantgreat24@gmail.com>
2020-09-10 00:31:42 +0530

    done

---
+++ power.java
@@ -0,0 +1,16 @@
+public class power {
+    public static void main(String[]args){
+    int x=4,y=1;
+    int power=1;
+    for(y=1;y<3;y++)
+    {
+        power=power*x;
+    }
+        System.out.println(power);
+    }
+
+}
+
+
+
+
