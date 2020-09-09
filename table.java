7f8e92168b9270332edbd1cd27fdc547ed0f097f
poochingil <siddhantgreat24@gmail.com>
2020-09-10 01:53:49 +0530

    done

---
+++ table.java
@@ -0,0 +1,10 @@
+public class table {
+    public static void main(String[]args){
+    int num=12,limit=20,i=1,sum=1;
+    for(i=1;i<=limit;i++)
+    {
+        sum=num*i;
+        System.out.println(sum);
+    }
+}
+}
