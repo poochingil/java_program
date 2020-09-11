913477f92ee287a82f6968d178fe1e1bbb2a9149
poochingil <siddhantgreat24@gmail.com>
2020-09-11 07:35:26 +0530

    done

---
+++ temperature.java
@@ -0,0 +1,8 @@
+public class temperature {
+    public static void main(String[]args)
+    {
+        int tempc=36;
+        float tempf=(9*tempc/5)+32;
+        System.out.println(tempf);
+    }
+}
