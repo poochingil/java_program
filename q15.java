62f369564fc26d1794c736481d179fcd6092c585
poochingil <siddhantgreat24@gmail.com>
2020-09-12 20:14:01 +0530

    done

---
+++ q15.java
@@ -0,0 +1,17 @@
+public class q15 {
+    public static void main(String[] args)
+    {
+        byte a=(byte)10;
+        a=(byte)(a+(byte)10);
+        System.out.println(a);
+        short b=(short)15;
+        short c=(short)(b/(short)3.0);
+        System.out.println(c);
+        double d=12.9;
+        float e= ((float)d/(float)3.5);
+        System.out.println(e);
+
+
+
+}
+}
