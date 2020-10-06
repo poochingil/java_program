5b34d25c887514cc33a3a71add83d9d9eddf38a4
poochingil <siddhantgreat24@gmail.com>
2020-10-07 00:26:10 +0530

    new project

---
+++ varlen.java/src/demo.java
@@ -0,0 +1,7 @@
+public class demo {
+    public static void main(String[] args){
+        xyz obj;
+        obj = new xyz();
+        int r = obj.add(10,20,39);
+    }
+}
---
+++ varlen.java/src/xyz.java
@@ -0,0 +1,13 @@
+public class xyz {
+    public int add(int...a)
+    {
+        int sum=0;
+        for(int x:a)
+        {
+            sum=sum+x;
+        }
+        System.out.println(sum);
+        return 0;
+    }
+}
+
